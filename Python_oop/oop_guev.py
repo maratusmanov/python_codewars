45ae984676703fcf1909d977bccdee790f119081
maratusmanov <fericogfdsa@gmail.com>
2024-02-27 12:52:54 +0300

    oop guev

--- Python_oop/oop_guev.py
+++ Python_oop/oop_guev.py
@@ -1335,3 +1335,162 @@
 #         if isinstance(n, int) or isinstance(n, float):
 #             return FoodInfo(self._proteins // n, self._fats // n, self._carbohydrates // n)
 #         return NotImplemented
+
+# Реализуйте класс Vector , описывающий вектор на плоскости. При создании
+# экземпляра класс должен принимать два аргумента в следующем порядке:
+# • x — координата вектора по оси x
+# • y — координата вектора по оси y
+# Экземпляр класса Vector должен иметь следующее формальное строковое
+# представление:
+# Vector(<координата x>, <координата y>)
+# Также экземпляры класса Vector должны поддерживать между собой операции
+# сложения и вычитания с помощью операторов + и - соответственно:
+# • результатом сложения должен являться новый экземпляр
+# класса Vector , координата по оси x которого равна сумме координат по
+# оси x исходных векторов, координата по оси y — сумме координат по
+# оси y исходных векторов
+# • результатом вычитания должен являться новый экземпляр
+# класса Vector координата по оси x которого равна разности координат по
+# оси x исходных векторов с учетом порядка, координата по оси y —
+# разности координат по оси y исходных векторов с учетом порядка
+# Наконец, экземпляр класса Vector должен поддерживать операции умножения
+# и деления на число n с помощью операторов * и / соответственно:
+# • результатом умножения должен являться новый экземпляр
+# класса Vector , координаты которого умножены на n
+# • результатом деления должен являться новый экземпляр
+# класса Vector , координаты которого поделены на n
+# Операция умножения должна быть выполнима независимо от порядка
+# операндов, то есть должна быть возможность умножить как вектор на число, так
+# и число на вектор.
+
+# class Vector:
+#     def __init__(self, x = 0, y = 0):
+#         self._x = x
+#         self._y = y
+#     def __str__(self):
+#          return f"Vector(<координата {self._x}><координата {self._y}>)"
+#     def __add__(self, other):
+#         if isinstance(other, Vector):
+#             return Vector(other._x + self._x, other._y + self._y)
+#         return NotImplemented
+#     def __sub__(self, other):
+#         if isinstance(other, Vector):
+#             return Vector(self._x - other._x, self._y - other._y)
+#         return NotImplemented
+#     def __mul__(self, other):
+#         if isinstance(other, (int, float)):
+#             return Vector(self._x * other, self._y * other)
+#         elif isinstance(other, Vector):
+#             return Vector(self._x * other._x, self._y * other._y)
+#         else:
+#             return NotImplemented
+#     def __rmul__(self, other):
+#         return self * other
+
+# Реализуйте класс SuperString , описывающий строку. При создании экземпляра
+# класс должен принимать один аргумент:
+# • string — значение строки
+# Экземпляр класса SuperString должен иметь следующее неформальное
+# строковое представление:
+# <значение строки>
+# Помимо этого, экземпляры класса SuperString должны поддерживать между
+# собой операцию сложения с помощью оператора + , результатом которой должен
+# являться новый экземпляр класса SuperString , представляющий
+# конкатенацию исходных.
+# Также экземпляр класса SuperString должен поддерживать операции
+# умножения, деления, побитового сдвига влево и побитового сдвига вправо на
+# целое число n с помощью операторов *, /, << и >> соответственно:
+# • результатом умножения должен являться новый экземпляр
+# класса SuperString , представляющий исходную строку, умноженную
+# на n
+# • результатом деления должен являться новый экземпляр
+# класса SuperString , представляющий строку из первых m символов
+# исходной строки, где m — длина исходной строки, поделенная нацело
+# на n
+# • результатом побитового сдвига влево должен являться новый экземпляр
+# класса SuperString , представляющий исходную строку без
+# последних n символов. Если n больше или равно длине исходной строки,
+# результатом должен являться экземпляр класса SuperString ,
+# представляющий пустую строку
+# • результатом побитового сдвига вправо должен являться новый экземпляр
+# класса SuperString , представляющий исходную строку без
+# первых n символов. Если n больше или равно длине исходной строки,
+# результатом должен являться экземпляр класса SuperString ,
+# представляющий пустую строку
+# Операция умножения должна быть выполнима независимо от порядка
+# операндов, то есть должна быть возможность умножить как строку на число, так
+# и число на строку
+# class SuperString:
+#     def __init__(self, string):
+#         self.string = string
+#     def __str__(self):
+#         return self.string
+#     def __add__(self, other):
+#         if isinstance(other, SuperString):
+#             return SuperString(self.string + other.string)
+#         return NotImplemented
+#     def __mul__(self, other):
+#         if isinstance(other, int):
+#             return SuperString(self.string * other)
+#         return NotImplemented
+#     def __rmul__(self, other):
+#         return self.__mul__(other)
+#     def __truediv__(self, other):
+#         if isinstance(other, int):
+#             return SuperString(self.string[:len(self.string) // other])
+#         return NotImplemented
+#     def __lshift__(self, other):
+#         if isinstance(other, int):
+#             if (length := len(self.string)) <= other:
+#                 return SuperString('')
+#             else:
+#                 return SuperString(self.string[0:length - other])
+#         return NotImplemented
+#     def __rshift__(self, other):
+#         if isinstance(other, int):
+#             if len(self.string) <= other:
+#                 return SuperString('')
+#             else:
+#                 return SuperString(self.string[other:])
+#         return NotImplemented
+
+# Реализуйте класс Time , описывающий время на цифровых часах. При создании
+# экземпляра класс должен принимать два аргумента в следующем порядке:
+# • hours — количество часов; каждые 24 часа должны преобразовываться
+# в 0 часов
+# • minutes — количество минут; каждые 60 минут должны
+# преобразовываться в 1 час
+# Экземпляр класса Time должен иметь следующее неформальное строковое
+# представление:
+# <количество часов в формате HH>:<количество минут в формате MM>
+# Также экземпляры класса Time должны поддерживать между собой операцию
+# сложения с помощью операторов + и += :
+# • результатом сложения с помощью оператора + должен являться новый
+# экземпляр класса Time , количество часов которого равно сумме
+# часов исходных экземпляров класса Time , количество минут — сумме
+# минут исходных экземпляров класса Time
+# • результатом сложения с помощью оператора += должен являться левый
+# экземпляр класса Time , количество часов которого увеличено на
+# количество часов правого экземпляра класса Time , количество минут —
+# на количество минут правого экземпляра класса Time
+#
+# class Time:
+#     def __init__(self, hours, minutes):
+#         self.hours, self.minutes = Time._normalize(hours, minutes)
+#     @staticmethod
+#     def _normalize(hours, minutes):
+#         return (hours + minutes // 60) % 24, minutes % 60
+#     def __str__(self):
+#         return f'{self.hours:>02}:{self.minutes:>02}'
+#     def __add__(self, other):
+#         if isinstance(other, Time):
+#             hours, minutes = self._normalize(self.hours + other.hours, self.minutes + other.minutes)
+#             return Time(hours, minutes)
+#         return NotImplemented
+#     def __iadd__(self, other):
+#         if isinstance(other, Time):
+#             self.hours, self.minutes = self._normalize(self.hours + other.hours, self.minutes + other.minutes)
+#             return self
+#         return NotImplemented
+
+#Todo: Магические методы. Вызываемые объекты
